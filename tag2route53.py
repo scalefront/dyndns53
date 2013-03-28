9fe7a5b0cd4ee05dd82c8e52c3168c776dfb3840
Brian McDonnell <mcdonnellb@gmail.com>
2013-03-28 18:34:01 +0000

    Record name now always ends with period.

--- tag2route53.py
+++ tag2route53.py
@@ -47,6 +47,8 @@ if __name__ == '__main__':
     pattern = re.compile("%s$" % args['truncate_name'])
     record_name = re.sub(pattern, "", instance_name)
     record_name = '.'.join((record_name, args['zone_domain_name']))
+    if not record_name.endswith('.'):
+        record_name += '.'
 
     # Determine the appropriate record value (pub/priv ip/dns_name)
     if args['record_type'] == 'A':
