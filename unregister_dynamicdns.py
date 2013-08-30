983d9c22618db68a70c76e1fe4d6caecd4069133
Brian McDonnell <mcdonnellb@gmail.com>
2013-08-30 16:49:57 +0100

    Can specify subdomain in unregister.

--- unregister_dynamicdns.py
+++ unregister_dynamicdns.py
@@ -27,17 +27,21 @@ if __name__ == '__main__':
     parser.add_argument('-p','--product-tld', help='Product TLD will be stripped from Name tag and replaced with Hosted Zone.', required=True, dest='product_tld')
     parser.add_argument('-z','--hosted-zone', help='Hosted Zone (e.g. r53-pub.example.com)', required=True, dest='hosted_zone')
     parser.add_argument('-r','--record-type', help='Type of DNS record to delete.', required=True, choices=['A', 'CNAME'], dest='record_type')
+    parser.add_argument('-s','--subdomain', help='Prefix a subdomain onto the record.', required=False, dest='subdomain')
     args = vars(parser.parse_args())
 
     from utils import get_current_instance_id, get_instance, replace_parent_domain
     instance_id = get_current_instance_id()
     instance = get_instance(instance_id)
     tag_name = instance.tags['Name']
-    new_domain_name = replace_parent_domain(tag_name, args['product_tld'], args['hosted_zone'])
+    domain_name = replace_parent_domain(tag_name, args['product_tld'], args['hosted_zone'])
+    if args['subdomain']:
+        from utils import join_domain
+        domain_name = join_domain(args['subdomain'], domain_name)
 
     import datetime
     print "--- %s (unregister) ---" % datetime.datetime.now()
     print "Instance Tag Name: %s" % tag_name
-    print "Record Name:       %s" % new_domain_name
+    print "Record Name:       %s" % domain_name
     print "Record Type:       %s" % args['record_type']
-    delete_record(new_domain_name, args['record_type'], args['hosted_zone'])
+    delete_record(domain_name, args['record_type'], args['hosted_zone'])
