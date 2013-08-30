575d2c075e7ef99ddafe6f28ad54342cdd235c5c
Brian McDonnell <mcdonnellb@gmail.com>
2013-08-30 16:58:26 +0100

    Extract get_hostname functionality into reusable function.

--- get_hostname.py
+++ get_hostname.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+
+def get_hostname(product_tld, hosted_zone, end_with_period=False):
+    from utils import get_current_instance_id, get_instance, replace_parent_domain
+    instance_id = get_current_instance_id()
+    instance = get_instance(instance_id)
+    tag_name = instance.tags['Name']
+    domain_name = replace_parent_domain(tag_name, args['product_tld'], args['hosted_zone'], end_with_period)
+    return domain_name
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description="Prints a hostname based on the current AWS instance's \"Name\" tag \
@@ -7,10 +16,5 @@ if __name__ == '__main__':
     parser.add_argument('-p','--product-tld', help='Product TLD will be stripped from Name tag and replaced with Hosted Zone.', required=True, dest='product_tld')
     parser.add_argument('-z','--hosted-zone', help='Hosted Zone (e.g. r53-pub.example.com)', required=True, dest='hosted_zone')
     args = vars(parser.parse_args())
-
-    from utils import get_current_instance_id, get_instance, replace_parent_domain
-    instance_id = get_current_instance_id()
-    instance = get_instance(instance_id)
-    tag_name = instance.tags['Name']
-    new_domain_name = replace_parent_domain(tag_name, args['product_tld'], args['hosted_zone'], end_with_period=False)
-    print new_domain_name
+    domain_name = get_hostname(args['product_tld'], args['hosted_zone'])
+    print domain_name
